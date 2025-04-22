e32b908471ef0ded512df9f9cd43d2c96aa88251
邹洁6 <2474179612@qq.com>
2025-04-22 17:12:36 +0800

    4.22-zj-会员页面展示

---
+++ common/model/user_power.go
@@ -0,0 +1,11 @@
+package model
+
+import "time"
+
+type Power struct {
+	Id      uint32    `gorm:"column:id;type:int UNSIGNED;primaryKey;not null;" json:"id"`
+	Name    string    `gorm:"column:name;type:varchar(30);comment:权益名称;not null;" json:"name"`                                                        // 权益名称
+	Grade   uint32    `gorm:"column:grade;type:int UNSIGNED;comment:会员等级：1-普通会员，2-青铜会员，3-黄铜会员，4-白银会员，5-黄金会员，6-钻石会员;not null;default:1;" json:"grade"` // 会员等级：1-普通会员，2-青铜会员，3-黄铜会员，4-白银会员，5-黄金会员，6-钻石会员
+	Explain string    `gorm:"column:explain;type:text;comment:说明;not null;" json:"explain"`                                                           // 说明
+	AddTime time.Time `gorm:"column:add_time;type:datetime;comment:添加时间;not null;default:CURRENT_TIMESTAMP;" json:"add_time"`                         // 添加时间
+}
