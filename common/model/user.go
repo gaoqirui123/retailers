a1dec87e3a145b8a91383317c7976603f7fbcb7c
邹洁6 <2474179612@qq.com>
2025-04-24 17:10:47 +0800

    asd

--- common/model/user.go
+++ common/model/user.go
@@ -13,16 +13,17 @@ type User struct {
 	Nickname       string  `gorm:"column:nickname;type:varchar(60);comment:用户昵称;not null;" json:"nickname"`                              // 用户昵称
 	Avatar         string  `gorm:"column:avatar;type:varchar(256);comment:用户头像;not null;" json:"avatar"`                                 // 用户头像
 	Phone          string  `gorm:"column:phone;type:char(15);comment:手机号码;default:NULL;" json:"phone"`                                   // 手机号码
-	NowMoney       float64 `gorm:"column:now_money;type:decimal(8, 2) UNSIGNED;comment:用户余额;not null;default:0.00;" json:"now_money"`    // 用户余额
+	FreezeAmount   float64 `gorm:"column:freeze_amount;type:decimal(8, 2);comment:冻结金额;default:NULL;" json:"freeze_amount"`              // 冻结金额
+	NowMoney       uint32  `gorm:"column:now_money;type:decimal(8, 2) UNSIGNED;comment:用户余额;not null;default:0.00;" json:"now_money"`    // 用户余额
 	BrokeragePrice float64 `gorm:"column:brokerage_price;type:decimal(8, 2);comment:佣金金额;not null;default:0.00;" json:"brokerage_price"` // 佣金金额
-	Integral       float64 `gorm:"column:integral;type:decimal(8, 2) UNSIGNED;comment:用户剩余积分;not null;default:0.00;" json:"integral"`    // 用户剩余积分
+	Integral       uint32  `gorm:"column:integral;type:decimal(8, 2) UNSIGNED;comment:用户剩余积分;not null;default:0.00;" json:"integral"`    // 用户剩余积分
 	SignNum        int32   `gorm:"column:sign_num;type:int;comment:连续签到天数;not null;default:0;" json:"sign_num"`                          // 连续签到天数
 	Status         int8    `gorm:"column:status;type:tinyint(1);comment:1为正常，0为禁止;not null;default:1;" json:"status"`                    // 1为正常，0为禁止
 	Level          uint8   `gorm:"column:level;type:tinyint UNSIGNED;comment:等级;not null;default:0;" json:"level"`                       // 等级
 	SpreadUid      uint32  `gorm:"column:spread_uid;type:int UNSIGNED;comment:推广元id;not null;default:0;" json:"spread_uid"`              // 推广元id
 	SpreadTime     uint32  `gorm:"column:spread_time;type:int UNSIGNED;comment:推广员关联时间;not null;default:0;" json:"spread_time"`          // 推广员关联时间
 	UserType       string  `gorm:"column:user_type;type:varchar(32);comment:用户类型;not null;" json:"user_type"`                            // 用户类型
-	IsPromoter     uint8   `gorm:"column:is_promoter;type:tinyint UNSIGNED;comment:是否为推广员;not null;default:0;" json:"is_promoter"`       // 是否为推广员
+	IsPromoter     uint8   `gorm:"column:is_promoter;type:tinyint UNSIGNED;comment:是否为推广员/1是2否;not null;default:1;" json:"is_promoter"`  // 是否为推广员/1是2否
 	PayCount       uint32  `gorm:"column:pay_count;type:int UNSIGNED;comment:用户购买次数;default:0;" json:"pay_count"`                        // 用户购买次数
 	SpreadCount    int32   `gorm:"column:spread_count;type:int;comment:下级人数;default:0;" json:"spread_count"`                             // 下级人数
 	CleanTime      int32   `gorm:"column:clean_time;type:int;comment:清理会员时间;default:0;" json:"clean_time"`                               // 清理会员时间
